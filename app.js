feaf9fa91c8985505810de5aa6f2c6a882efc9ff
thasup <thanachonfirst@hotmail.com>
2022-04-15 22:42:11 +0700

    Add: slider inputs

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 Vue.createApp({
   data() {
     return {
-      message: "Hello Vue!",
+      perspective: 0,
+      rotateX: 0,
+      rotateY: 0,
+      rotateZ: 0,
     };
+  },
+  methods: {
+    reset() {
+      this.perspective = 0;
+      this.rotateX = 0;
+      this.rotateY = 0;
+      this.rotateZ = 0;
+    },
   },
 }).mount("#app");
