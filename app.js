35fc8440cc01179105abd4b51d6e01c279df2bbd
thasup <thanachonfirst@hotmail.com>
2022-04-15 23:09:05 +0700

    Add: functionality to app

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 Vue.createApp({
   data() {
     return {
-      perspective: 0,
+      perspective: 100,
       rotateX: 0,
       rotateY: 0,
       rotateZ: 0,
@@ -9,10 +9,22 @@ Vue.createApp({
   },
   methods: {
     reset() {
-      this.perspective = 0;
+      this.perspective = 100;
       this.rotateX = 0;
       this.rotateY = 0;
       this.rotateZ = 0;
+    },
+  },
+  computed: {
+    box() {
+      return {
+        transform: `
+          perspective(${this.perspective}px)
+          rotateX(${this.rotateX}deg)
+          rotateY(${this.rotateY}deg)
+          rotateZ(${this.rotateZ}deg)
+        `,
+      };
     },
   },
 }).mount("#app");
